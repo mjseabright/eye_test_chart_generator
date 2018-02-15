5b59c6b738bd5c15d90fd44b3f84eec06b15d9f4
Matthew <mjseabright@gmail.com>
2018-02-15 20:36:37 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eye_test_chart_generator
---
+++ scripts.js
@@ -0,0 +1,24 @@
+var numRows = 11;
+var maxSize = 100;
+var minSize = 16
+
+function generateChart() {
+	maxSize = document.getElementById("maxSizeInput").value;
+	minSize = document.getElementById("minSizeInput").value;
+	var tempHTML = "";
+	for (var row = 0; row < numRows; row++) {
+		var size = (maxSize - (row * (maxSize - minSize) / (numRows - 1)));
+		tempHTML += "<p style=\"line-height: 40%;\"><span style=\"font-size:" + size + "%;\">" + generateRandomString(5) + "</span>\n";
+		tempHTML += "<span style=\"float:right;font-size:40px;font-family:Arial\">" + (row + 1) + " - " + (Math.round(size* 100) / 100) + "</span></p>";
+	}
+	document.getElementById("chart").innerHTML = tempHTML;
+}
+
+function generateRandomString(length) {
+	var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var randomString = "";
+	for (var i = 0; i < length; i++) {
+		randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return randomString;
+}
